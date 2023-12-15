4ed0aac88018af924e5eef64175034499de27820
nhu.ddq <nhu.ddq@DLAP-0479.clv.local>
2023-12-15 11:17:31 +0700

    BasicJava 1st commit

---
+++ src/Basicday2/Day4.java
@@ -0,0 +1,12 @@
+package Basicday2
+;
+
+public class Day4 {
+public static void main(String[] args) {
+	String name = "Nhu";
+	String tensach ;
+	tensach ="laptrinhvien";
+	System.out.println("Nhu");
+	System.out.println(tensach);
+}
+}
