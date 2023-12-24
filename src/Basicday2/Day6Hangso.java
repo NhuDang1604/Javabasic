70c6f5ab5d6217daa9ff2151c31787ae00f9d342
nhu.ddq <nhu.ddq@DLAP-0479.clv.local>
2023-12-24 14:24:11 +0700

    Z

---
+++ src/Basicday2/Day6Hangso.java
@@ -0,0 +1,16 @@
+package Basicday2;
+
+public class Day6Hangso {
+	public static void main(String[] args) {
+		String tenSach = "Lap trinh Java";
+		int namXuatBan = 2021;
+		double giaTien = 15.5;
+		boolean conSach = true;
+		char maKho = 'M';
+		System.out.println("Ten sach: "+ tenSach);
+		System.out.println("Nam: "+ namXuatBan);
+		System.out.println("Gia: "+ giaTien);
+		System.out.println("Con sach: "+ conSach);
+		System.out.println("Ma kho: "+ maKho);
+	}
+}
